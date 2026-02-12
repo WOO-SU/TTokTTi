a9b45c8ad8b2bb714d6fa2a9ee099731cc47e09e
igotyabingo <jenifer7933@snu.ac.kr>
2026-02-12 15:38:01 +0900

    feat: add more APIs for team

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import LoginView, logout, change_password
+from .views import LoginView, logout, change_password, coworker, search_user_by_birth
 
 app_name = "user"
 
@@ -10,5 +10,6 @@ urlpatterns = [
     path("logout/", logout, name="logout"),
     path("password/change/", change_password, name="password_change"),
     path("storage/", include("apps.user.storage.urls")),
-
+    path("coworker/", coworker, name="coworker"),
+    path("search/", search_user_by_birth, name="search_user_by_birth"),
 ]
