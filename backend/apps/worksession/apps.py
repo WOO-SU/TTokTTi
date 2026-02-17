d3d7e26fa490a940ab1273ea40c4bd1e8b0c0def
igotyabingo <jenifer7933@snu.ac.kr>
2026-02-17 20:31:57 +0900

    fix: update models.py for detect app

--- backend/apps/worksession/apps.py
+++ backend/apps/worksession/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class WorksessionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'worksession'
+    name = 'apps.worksession'
