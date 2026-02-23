5e0cb5ee34eacde2b32c0a0d68bddbfda627a092
jaesunglee9 <ljaesung1130@gmail.com>
2026-02-23 20:12:04 +0900

    Add comma at urls.py

--- backend/apps/detect/urls.py
+++ backend/apps/detect/urls.py
@@ -5,6 +5,6 @@ app_name = "detect"
 urlpatterns = [
     path("save/", save_video, name="save_video"), 
     path("search/", search_video, name="search_video"),
-    path("bodycam/", bodycam_risk, name="bodycam_risk")
+    path("bodycam/", bodycam_risk, name="bodycam_risk"),
     path("admin/logs/", check_logs, name="check_logs"),
 ]
